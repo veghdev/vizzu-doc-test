8796bf337e3a3c15d8ce23b6d5c5800ef00f405f
David Vegh <david.andras.vegh@gmail.com>
2023-06-08 10:29:01 +0100

    Deployed ae204cb to 0.4 with MkDocs 1.4.3 and mike 1.2.0.dev0

--- 0.4/assets/javascripts/plausible.js
+++ 0.4/assets/javascripts/plausible.js
@@ -1,20 +1,20 @@
-const plausibleScriptElement1 = document.createElement("script");
-plausibleScriptElement1.setAttribute("defer", "");
-plausibleScriptElement1.setAttribute("data-domain", "veghdev.github.io");
-plausibleScriptElement1.src = "https://plausible.io/js/script.outbound-links.js";
+let plausibleHead1 = document.createElement("script");
+plausibleHead1.setAttribute("defer", "");
+plausibleHead1.setAttribute("data-domain", "veghdev.github.io");
+plausibleHead1.src = "https://plausible.io/js/script.outbound-links.js";
 
-const plausibleScriptElement2 = document.createElement("script");
-plausibleScriptElement2.textContent =
+let plausibleHead2 = document.createElement("script");
+plausibleHead2.textContent =
   "window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }";
 
-const plausibleHeadElement = document.getElementsByTagName("head")[0];
-plausibleHeadElement.appendChild(plausibleScriptElement1);
-plausibleHeadElement.appendChild(plausibleScriptElement2);
+let headElement = document.getElementsByTagName("head")[0];
+headElement.appendChild(plausibleHead1);
+headElement.appendChild(plausibleHead2);
 
 if (window.location.pathname.indexOf("404.html") !== -1) {
-  const plausibleScriptElement404 = document.createElement("script");
-  plausibleScriptElement404.textContent =
+  let plausibleBody1 = document.createElement("script");
+  plausibleBody1.textContent =
     'plausible("404",{ props: { path: document.location.pathname } });';
 
-  document.body.appendChild(plausibleScriptElement404);
+  document.body.appendChild(plausibleBody1);
 }
