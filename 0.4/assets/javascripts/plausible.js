52f612695cf47a8e961980783adfdf5791e9e751
David Vegh <david.andras.vegh@gmail.com>
2023-06-08 10:38:41 +0100

    Deployed ae204cb to 0.4 with MkDocs 1.4.3 and mike 1.2.0.dev0

--- 0.4/assets/javascripts/plausible.js
+++ 0.4/assets/javascripts/plausible.js
@@ -1,7 +1,8 @@
 let plausibleHead1 = document.createElement("script");
 plausibleHead1.setAttribute("defer", "");
 plausibleHead1.setAttribute("data-domain", "veghdev.github.io");
-plausibleHead1.src = "https://plausible.io/js/script.outbound-links.js";
+plausibleHead1.src =
+  "https://plausible.io/js/script.outbound-links.js";
 
 let plausibleHead2 = document.createElement("script");
 plausibleHead2.textContent =
@@ -10,3 +11,11 @@ plausibleHead2.textContent =
 let headElement = document.getElementsByTagName("head")[0];
 headElement.appendChild(plausibleHead1);
 headElement.appendChild(plausibleHead2);
+
+if (window.location.pathname.indexOf("404.html") !== -1) {
+  let plausibleBody1 = document.createElement("script");
+  plausibleBody1.textContent =
+    'plausible("404",{ props: { path: document.location.pathname } });';
+
+  document.body.appendChild(plausibleBody1);
+}
