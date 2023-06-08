634fd0dda4169ac751bb1fc46c770d44ae397d68
David Vegh <david.andras.vegh@gmail.com>
2023-06-08 14:14:50 +0200

    test

--- 0.4/assets/javascripts/plausible/body.js
+++ 0.4/assets/javascripts/plausible/body.js
@@ -1,7 +1,9 @@
 if (window.location.pathname.indexOf("404.html") !== -1) {
+  console.log(document.location.pathname)
   const plausible404 = document.createElement("script");
   plausible404.textContent =
     'plausible("404",{ props: { path: document.location.pathname } });';
 
   document.body.appendChild(plausible404);
 }
+
